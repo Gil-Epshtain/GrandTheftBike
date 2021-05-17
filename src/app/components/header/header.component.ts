32914b18ec2dc75dd5fbd98f8921c6bf06010aa6
Gil Epshtain <gil_epshtain@yahoo.com>
2021-05-17 17:19:45 +0300

    Header - clicking logo will redirect to Home

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HeaderComponent implements OnInit
+{
+  public constructor(
+    private _router: Router)
+  {
+    console.log("Header.component - ctor");
   }
 
+  public ngOnInit(): void
+  {
+  }
+
+  public onClick_Header(): void
+  {
+    this._router.navigate(['']); // navigate to homepage
+  }
 }
